7c818cc8d0f951eabab812decbcd9ee0c6e773eb
2387672 <2387672@LTIN521537>
2025-03-26 10:46:40 +0530

    Add project files.

---
+++ WebapiProject/Models/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebapiProject.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Stock> Stocks { get; set; }
+     public DbSet<StockDto> ProductDtos { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Report> Reports { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<StockDto>().HasNoKey();
+        }
+
+    }
+}
